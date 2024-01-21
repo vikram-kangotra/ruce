1c133c7e32cb2c4c7cef46f99371c496920523f5
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-01-21 19:23:29 +0530

    No need for build script and searching for all rust files

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 EMCC=emcc
 
 main: main.c libruce
-	$(EMCC) main.c output.c -lruce -L. -o main.html
+	$(EMCC) main.c js_code.c -lruce -L. -o main.html
 
 libruce:
 	cargo build --target wasm32-unknown-emscripten
 	cp target/wasm32-unknown-emscripten/debug/libruce.a .
 
 clean:
-	rm output.c main.html main.js main.wasm libruce.a
+	rm js_code.c main.html main.js main.wasm libruce.a
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
 use ruce_macro::js_code;
 
 #[js_code]
-fn console_log() {
-    console.log("Hello, world!");
+fn console_log() { 
+    console.log("Hello, world 1!"); 
+    console.log("Hello, world 2!"); 
 }
 
 #[js_code]
-fn alert() {
-    alert("Success!");
+fn alert() { 
+    alert("Success!"); 
 }
 
 #[no_mangle]
 fn run() {
 
-    unsafe {
-        for _ in 0..10 {
-            console_log();
-        }
-
-        alert();
+    for _ in 0..10 {
+        console_log();
     }
+
+    alert();
 }
